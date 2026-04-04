b320ac9051065def91624232be634ccc53bbd254
Javed0323 <j43430749@gmail.com>
2026-04-04 20:34:24 +0500

    fix database URL env variable

--- database.py
+++ database.py
@@ -1,9 +1,11 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Format: postgresql://user:password@localhost/db_name
-URL = "postgresql://postgres:postgres@localhost/cars_db"
+URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost/cars_db")
 engine = create_engine(URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
