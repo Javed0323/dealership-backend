105c4d2433698debaf08f0edb031cbf9719f9eb1
Javed0323 <j43430749@gmail.com>
2026-04-04 20:59:51 +0500

    add load_dotenv to database.py

--- database.py
+++ database.py
@@ -1,16 +1,16 @@
 import os
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-# Format: postgresql://user:password@localhost/db_name
+load_dotenv()
+
 URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost/cars_db")
 engine = create_engine(URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# This is a 'Dependency' we'll use in our routes
 def get_db():
     db = SessionLocal()
     try:
